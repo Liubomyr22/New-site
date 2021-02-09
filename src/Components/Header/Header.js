ec5fa268c1d5452dbbdc096a7c6b4591d7685b23
Liubomyr22 <70060037+Liubomyr22@users.noreply.github.com>
2021-02-09 13:22:27 +0200

    changed styles

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import s from './Header.module.css'
+import HeaderForm from './HeaderForm';
+import Ava from './img/ava.jpg';
 
 const Header = () => {
     return <header className={s.header}>
+    <div className={s.logo}>
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv7wjBbM2LrBsbf9SrqN3OFOXLZIQ_2BAf3A&usqp=CAU" />
+    </div>
+    <div className={s.headerForm}>
+        <HeaderForm />
+    </div>
+    <div className={s.headerAva}>
+        <img src={Ava} alt="Avatar" />
+        <li>Online</li>
+    </div>
     </header>
 }
 
-export default Header
+export default Header;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import s from './Nav.module.css';
+import NavAvaAndInfo from "./NavAvaAndInfo";
 
 const Nav = () => {
   return (
     <nav className={s.nav}>
+     <NavAvaAndInfo />
       <div className={s.item}>
-        <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
+        <NavLink to="/profile"  activeClassName={s.activeLink}><button className={s.glowOnHover}>Profile</button></NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
+        <NavLink to="/dialogs" activeClassName={s.activeLink}><button className={s.glowOnHover}>Messages</button></NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
+        <NavLink to="/users" activeClassName={s.activeLink}><button className={s.glowOnHover}>Users</button></NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
+        <NavLink to="/news" activeClassName={s.activeLink}><button className={s.glowOnHover}>News</button></NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/music" activeClassName={s.activeLink}>Music</NavLink>
+        <NavLink to="/music" activeClassName={s.activeLink}><button className={s.glowOnHover}>Music</button></NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink>
+        <NavLink to="/settings" activeClassName={s.activeLink}><button className={s.glowOnHover}>Settings</button></NavLink>
       </div>
     </nav>
   );
